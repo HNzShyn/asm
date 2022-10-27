e90b123cfc3670e3ca738d80e2818f71f4a333e6
HNzShyn <nvu1343@gmail.com>
2022-10-27 13:40:00 +0700

    ok

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 app.listen(port);
 
 module.exports = app;
